16d2df302e3aef134f28cc97d37466c103f5f2ea
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-27 13:21:30 +0530

    greeting

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -8,7 +8,7 @@ export default function HOME() {
 
   return user !== null ? (
     <div className='flex flex-col flex-1 items-center justify-center font-sans  w-full min-h-screen'>
-      {user.name}
+      hello {user.name} 👋
     </div>
   ) : (
     <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans'>
