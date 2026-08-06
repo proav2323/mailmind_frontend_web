e63cecf37d09037171d49fb6ff9ac6ab7c99eb25
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-08-06 23:09:54 +0530

    syncing-emails

---
+++ app/components/SyncingNewEmails.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useUser } from "../states/user";
+import Loader from "./loader";
+
+export default function SyncToast() {
+  const { user } = useUser();
+  return (
+    <div
+      className={`p-4 h-fit w-[20vw] rounded-md bg-[var(--bg-secondary)] absolute top-2 right-2 gap-2 flex flex-row justify-center items-center animate-bounce transition ease-in-out ${user !== null && user.emails.length >= 1 ? "opacity-100" : "opacity-0"}`}
+    >
+      <div className='h-full'>
+        <Loader />
+      </div>
+      <span className={`font-bold`}>Syncing new Emails</span>
+    </div>
+  );
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import EmailInitializer from "../components/EmailIntiazer";
+import { useGlobalSocket } from "../components/SocketContext";
+import { useEffect, useState } from "react";
+import SyncToast from "../components/SyncingNewEmails";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,12 +20,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const SOCKET = useGlobalSocket();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (SOCKET.isConnected && SOCKET.socket) {
+      SOCKET.socket.on("emailLoading", () => {
+        setIsLoading(true);
+      });
+
+      SOCKET.socket.on("newEmail", () => {
+        setIsLoading(false);
+      });
+    }
+  }, [SOCKET.isConnected, SOCKET.socket]);
   return (
     <html
       lang='en'
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className='min-h-full flex flex-col'>
+        {isLoading ? <SyncToast /> : null}
         {children}
         <EmailInitializer />
       </body>
