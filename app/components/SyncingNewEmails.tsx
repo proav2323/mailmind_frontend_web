c2191ca1c37f21c57b67a14403f8696fd5a4099d
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-08-14 20:45:22 +0530

    syncing_responsive

--- app/components/SyncingNewEmails.tsx
+++ app/components/SyncingNewEmails.tsx
@@ -7,7 +7,7 @@ export default function SyncToast() {
   const { user } = useUser();
   return (
     <div
-      className={`z-100 p-4 h-fit w-[20vw] rounded-md bg-[var(--bg-secondary)] absolute top top-2 right-2 gap-2  transition-all ease-in-out flex flex-row justify-center items-center ${user !== null ? "flex" : "hidden"}`}
+      className={`z-100 p-4 h-fit w-[80vw] md:w-[50vw] lg:w-[20vw] rounded-md bg-[var(--bg-secondary)] absolute top top-2 right-2 gap-2  transition-all ease-in-out flex flex-row justify-center items-center ${user !== null ? "flex" : "hidden"}`}
     >
       <div className='h-full'>
         <Loader />
