6a29c1bd5ffd5e46c6057fe99ce2189f97032283
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-08-18 19:22:23 +0530

    scrollbar-none

--- app/pages/email.tsx
+++ app/pages/email.tsx
@@ -156,7 +156,7 @@ export default function Email({ email }: { email: EMAIL | null }) {
                 <div
                   key={idx}
                   dangerouslySetInnerHTML={{ __html: cleanHtml }}
-                  className='w-[95%] overflow-x-scroll scollbar-none'
+                  className='w-[95%] overflow-x-scroll scrollbar-none'
                 />
               );
             })}
